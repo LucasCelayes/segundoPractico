c980d83255c369a98392b6fcb4330c9debef4f60
LucasCelayes <93890374+LucasCelayes@users.noreply.github.com>
2024-06-03 23:57:18 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp1.Database;
+using System.Data.SqlClient;
+
+namespace ConsoleApp1.Models
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GestorBaseDeDatos gestorBaseDeDatos = new GestorBaseDeDatos();
+
+            try 
+            {
+                // if(gestorBaseDeDatos.DeleteUser(10))
+                //{
+                //  Console.WriteLine("usuario eliminado");
+                //}
+                Usuario newUser = new Usuario("Jonathan", "Rodriguez", "jona22", "123456789", "jonaro@gmail.com");
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
